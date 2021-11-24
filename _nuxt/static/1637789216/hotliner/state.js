b13bad5787da908f221d3dc88a258520844cfee8
jonathanbergson <auth@bergson.me>
2021-11-24 21:27:08 +0000

    Deploying to dist from @ jonathanbergson/jonathanbergson.github.io@9d30f449c180896ccac08da7c582cc9741eead6f 🚀

--- _nuxt/static/1637789216/hotliner/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637789216",layout:"blog",error:a,serverRendered:true,routePath:"\u002Fhotliner",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"eb36bf9d"}}}}(null));
---
+++ _nuxt/static/1637789225/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fvenda-sertao-mundo","\u002Fhotliner","\u002Fpandora"]})
