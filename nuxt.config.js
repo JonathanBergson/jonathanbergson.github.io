3009900ae9b75da73d6aeda84fe76eb46abce0df
Jonathan Bergson <jonathan@track.co>
2021-11-24 17:48:03 -0300

    fix: gerando arquivos das páginas

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,6 +45,14 @@ export default {
     '@nuxt/content',
   ],
 
+  generate: {
+    async routes() {
+      const {$content} = require("@nuxt/content")
+      const files = await $content({deep:true}).only(['path']).fetch()
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
+  },
+
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
