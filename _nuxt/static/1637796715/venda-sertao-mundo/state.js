1fca651690ec8e963b7c586c82e7c7c6f2cff96d
jonathanbergson <auth@bergson.me>
2021-11-24 23:37:42 +0000

    Deploying to dist from @ jonathanbergson/jonathanbergson.github.io@10cd6ce1b501c12ef418d5a2d9e00ed3b0d4402e 🚀

--- _nuxt/static/1637796715/venda-sertao-mundo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637796715",layout:"blog",error:a,serverRendered:true,routePath:"\u002Fvenda-sertao-mundo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f0f2039b"}}}}(null));
