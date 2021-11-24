f60a3f96965cf81cba603f7fb29d4a78a175c17b
Jonathan Bergson <auth@bergson.me>
2021-11-24 18:15:09 -0300

    Create _slug.vue

---
+++ pages/games/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    <h1>{{ document.title }}</h1>
+    <nuxt-content :document="document" />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "blog",
+  async asyncData({$content, params}) {
+    const document = await $content(params.slug || 'index').fetch()
+    return { document }
+  }
+}
+</script>
