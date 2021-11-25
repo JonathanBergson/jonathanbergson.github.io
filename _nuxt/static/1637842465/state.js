25ff9a2540dbb7da60636d1ab2dfa5134af9871c
jonathanbergson <auth@bergson.me>
2021-11-25 12:34:43 +0000

    Deploying to dist from @ jonathanbergson/jonathanbergson.github.io@ae776a624694baf6814ede9e823b4deac094c789 🚀

--- _nuxt/static/1637842465/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637842465",layout:"blog",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ad178dfd"}}}}(null,"\u002F"));
