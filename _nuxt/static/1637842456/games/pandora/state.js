c81d979f8609f8654b55d8d460f1de1cd6621fb9
jonathanbergson <auth@bergson.me>
2021-11-25 12:14:26 +0000

    Deploying to dist from @ jonathanbergson/jonathanbergson.github.io@904b38bfd1d716b7c2c1796b6a6ae83868df7630 🚀

--- _nuxt/static/1637842456/games/pandora/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637842456",layout:"blog",error:a,serverRendered:true,routePath:"\u002Fgames\u002Fpandora",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"816a9cc8"}}}}(null));
--- _nuxt/static/1637842456/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637842456",layout:"blog",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"816a9cc8"}}}}(null,"\u002F"));
